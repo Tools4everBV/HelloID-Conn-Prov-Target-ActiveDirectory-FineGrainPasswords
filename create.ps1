8a5318ec7205c004504d7730c02f345b6d1d4065
Mike Sheldon <m.sheldon@tools4ever.com>
2021-03-05 09:26:14 -0800

    Standardization

--- create.ps1
+++ create.ps1
@@ -1,53 +1,122 @@
-#Initialize default properties
-$p = $person | ConvertFrom-Json;
-$success = $False;
-$auditMessage = "for person " + $p.DisplayName;
+#region Initialize default properties
+$config = ConvertFrom-Json $configuration
+$p = $person | ConvertFrom-Json
+$pp = $previousPerson | ConvertFrom-Json
+$pd = $personDifferences | ConvertFrom-Json
+$m = $manager | ConvertFrom-Json
+
+$success = $False
+$auditLogs = New-Object Collections.Generic.List[PSCustomObject];
+
+#Get Primary Domain Controller
+$pdc = (Get-ADForest | Select-Object -ExpandProperty RootDomain | Get-ADDomain | Select-Object -Property PDCEmulator).PDCEmulator
+#endregion Initialize default properties
  
-#Specify DC
-$DC = "<DC FQDN>
- 
-#Get AD Account
-$account = Get-ADUser -Filter ('employeeID -eq "' + $p.externalId + '"') -Server $DC;
- 
-#Fine-Grain Password Group
-$group = "<POLICY GROUP NAME>";
- 
-#Password
-$Length = 16
-$nonAlphaChars = 5
-$password = [System.Web.Security.Membership]::GeneratePassword($length, $nonAlphaChars)
- 
-try {
- 
-    if(-Not($dryRun -eq $True)) {
-        Add-ADGroupMember -Identity $group -Members $account -Server $DC -Confirm:$false;
-        Set-ADAccountPassword -Identity $account -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $password -Force) -Server $DC -Confirm:$false;
-        $response = $true;
+#region Change mapping here
+    #Correlation
+    $correlationPersonField = ($config.correlationPersonField | Invoke-Expression)
+    $correlationAccountField = $config.correlationAccountField
+
+    #Password Config
+    $poiicyGroup = "<POLICY GROUP NAME>";
+    $length = 16
+    $nonAlphaChars = 5
+    $defaultPassword = [System.Web.Security.Membership]::GeneratePassword($length, $nonAlphaChars)
+
+#endregion Change mapping here
+
+#region Execute
+try{
+        #Find AD Account by attribute/value
+        try{
+            $filter = "($($correlationAccountField)=$($correlationPersonField))";
+            Write-Information "LDAP Filter: $($filter)";
+            
+            $account = Get-ADUser -LdapFilter $filter -Property sAMAccountName -Server $pdc
+            
+            if($account -eq $null) { throw "Failed to return a account" }
+
+            Write-Information "Account correlated to $($account.sAMAccountName)";
+
+            $auditLogs.Add([PSCustomObject]@{
+                        Action = "CreateAccount"
+                        Message = "Account correlated to $($account.sAMAccountName)";
+                        IsError = $false;
+                    });
+        } catch {
+            $auditLogs.Add([PSCustomObject]@{
+                Action = "CreateAccount"
+                Message = "Account failed to correlate:  $_"
+                IsError = $True
+            });
+            throw $_;
+        }
+
+        #Apply Password Policy Group
+        if(-Not($dryRun -eq $True)){
+            try{  
+                Add-ADGroupMember -Identity $policyGroup -Members $account -Server $pdc -Confirm:$false;
+                Write-Information "Applied $($policyGroup) group to $($account.sAMAccountName)";
+
+                $auditLogs.Add([PSCustomObject]@{
+                    Action = "UpdateAccount"
+                    Message = "Added $($account.sAMAccountName) to password policy group $($policyGroup)";
+                    IsError = $false;
+                });
+            }catch {
+                $auditLogs.Add([PSCustomObject]@{
+                    Action = "UpdateAccount"
+                    Message = "Failed to add $($account.sAMAccountName) to password policy group $($policyGroup): $($_)";
+                    IsError = $true;
+                });
+                throw $_;
+            }
+        } else { Write-Information "Password Policy $($policyGroup) would be applied" }
+
+        #Reset Password
+        if(-Not($dryRun -eq $True)){
+            try {
+                Set-ADAccountPassword -Identity $account -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $defaultPassword -Force) -Server $pdc -Confirm:$false;
+                Write-Information "Applied $($policyGroup) group to $($account.sAMAccountName)";
+
+                $auditLogs.Add([PSCustomObject]@{
+                    Action = "UpdateAccount"
+                    Message = "Reset Password for $($account.sAMAccountName) ";
+                    IsError = $false;
+                });
+            } catch {
+                $auditLogs.Add([PSCustomObject]@{
+                    Action = "UpdateAccount"
+                    Message = "Failed to Reset Password for $($account.sAMAccountName): $($_)";
+                    IsError = $true;
+                });
+                throw $_;
+            }
+        } else { Write-Information "Reset Password $($defaultPassword) would be applied" }
+
+        $success = $true;
     }
-    else
-    {
-        $response = $true;
+    catch {
+        Write-Error $_;
     }
-     
-    if($response -eq $true)
-    {
-        $success = $True;
-        $auditMessage = "Added " + $p.DisplayName + " to password policy group " + $group + " and reset password";
-    }
-     
 }
 catch
 {
-        $auditMessage = $_.toString() + " : General error"
+	Write-Error $_;
 }
- 
-#build up result
+#endregion Execute
+
+#region build up result
 $result = [PSCustomObject]@{
     Success= $success;
-    AccountReference= $account_guid;
-    AuditDetails=$auditMessage;
+    AccountReference= $account.SID.Value
+    AuditLogs = $auditLogs
     Account = $account;
+    ExportData = [PSCustomObject]@{
+        sAMAccountName = $account.sAMAccountName;
+        policyGroup = $policyGroup;
+    }
 };
- 
-#send result back
+
 Write-Output $result | ConvertTo-Json -Depth 10
+#endregion build up result
